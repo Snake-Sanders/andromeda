1f568f00e125c0f79a4ea9aa8e67472e5d6c3808
Snake Sanders <elmarian22@gmail.com>
2025-03-29 11:42:51 +0100

    moved content to functions

--- lib/andromeda_web/controllers/page_html.ex
+++ lib/andromeda_web/controllers/page_html.ex
@@ -7,4 +7,30 @@ defmodule AndromedaWeb.PageHTML do
   use AndromedaWeb, :html
 
   embed_templates "page_html/*"
+
+  def galaxy_bg(assigns) do
+    ~H"""
+    <div class="bg-[url(/images/andromeda-bg.webp)] bg-cover bg-center h-full w-full">
+      <div class="flex items-center justify-center h-full text-white text-4xl font-bold"></div>
+    </div>
+    """
+  end
+
+  def tooling_version(assigns) do
+    ~H"""
+    <div>
+      <h1 class=" mt-10 flex items-center text-sm  leading-6">
+        <p>
+          Page build with
+          <spam class="text-brand font-semibold">
+            Phoenix Framework
+            <small class="bg-brand/5 text-[0.8125rem] ml-2 rounded-full px-2 font-medium leading-6">
+              v{Application.spec(:phoenix, :vsn)}
+            </small>
+          </spam>
+        </p>
+      </h1>
+    </div>
+    """
+  end
 end
